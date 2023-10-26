bff42f346d9344fbe3d673ac5e0a07f0b80ff920
webberbond <sergey.zrch@gmail.com>
2023-10-26 17:59:45 +0500

    Added Rating Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,21 @@
-import { Button, Htag, P, Tag } from '../components';
+import { useEffect, useState } from 'react';
+import { Button, Htag, P, Rating, Tag } from '../components';
 
 export default function Home() {
+  const [counter, setCounter] = useState<number>(0);
+  const [rating, setRating] = useState<number>(4);
+
+  useEffect(() => {
+    console.log('Counter' + counter);
+    return function cleanup() { 
+      console.log('unmount');
+    };
+  });
+
   return (
     <div>
-      <Htag tag="h1">Text</Htag>
-      <Button appereance="primary" arrow="right">Кнопка</Button>
+      <Htag tag="h1">{counter}</Htag>
+      <Button appereance="primary" arrow="right" onClick={() => setCounter(x => x + 1)}>Кнопка</Button>
       <Button appereance="ghost" arrow="down">Кнопка</Button>
       <P size="l">Большой</P>
       <P>Средний</P>
@@ -13,6 +24,7 @@ export default function Home() {
       <Tag size="m" color="red">Red</Tag>
       <Tag size="s" color="green">Green</Tag>
       <Tag color="primary">Primary</Tag>
+      <Rating rating={rating} isEditable setRating={setRating} />
     </div>
   );
 }
