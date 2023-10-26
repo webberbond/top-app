30d07eb860f3e7bd6b47f471105e164ba975285a
webberbond <sergey.zrch@gmail.com>
2023-10-26 16:19:03 +0500

    Added Paragraph Component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,14 @@
-import { Button, Htag } from '../components';
+import { Button, Htag, P } from '../components';
 
 export default function Home() {
   return (
     <div>
       <Htag tag="h1">Text</Htag>
-      <Button appereance="primary" arrow='right'>Кнопка</Button>
-      <Button appereance="ghost" arrow='down'>Кнопка</Button>
+      <Button appereance="primary" arrow="right">Кнопка</Button>
+      <Button appereance="ghost" arrow="down">Кнопка</Button>
+      <P size="l">Большой</P>
+      <P>Средний</P>
+      <P size="s">Маленький</P>
     </div>
   );
 }
