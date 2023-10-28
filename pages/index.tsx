e703d89228e10c2597464535249a21328cbd9f3d
webberbond <sergey.zrch@gmail.com>
2023-10-28 19:58:06 +0500

    Added context & Menu layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,13 @@ import { GetStaticProps } from 'next';
 import axios from 'axios';
 import { MenuItem } from '@/interfaces/menu.interface';
 
-
 function Home({ menu }: HomeProps) {
   const [counter, setCounter] = useState<number>(0);
   const [rating, setRating] = useState<number>(4);
 
   useEffect(() => {
     console.log('Counter' + counter);
-    return function cleanup() { 
+    return function cleanup() {
       console.log('unmount');
     };
   });
@@ -20,17 +19,28 @@ function Home({ menu }: HomeProps) {
   return (
     <>
       <Htag tag="h1">{counter}</Htag>
-      <Button appereance="primary" arrow="right" onClick={() => setCounter(x => x + 1)}>Кнопка</Button>
-      <Button appereance="ghost" arrow="down">Кнопка</Button>
+      <Button
+        appereance="primary"
+        arrow="right"
+        onClick={() => setCounter((x) => x + 1)}
+      >
+        Кнопка
+      </Button>
+      <Button appereance="ghost" arrow="down">
+        Кнопка
+      </Button>
       <P size="l">Большой</P>
       <P>Средний</P>
       <P size="s">Маленький</P>
       <Tag size="s">Ghost</Tag>
-      <Tag size="m" color="red">Red</Tag>
-      <Tag size="s" color="green">Green</Tag>
+      <Tag size="m" color="red">
+        Red
+      </Tag>
+      <Tag size="s" color="green">
+        Green
+      </Tag>
       <Tag color="primary">Primary</Tag>
       <Rating rating={rating} isEditable setRating={setRating} />
-      <ul>{menu.map(m => (<li key={m._id.secondCategory}>{m._id.secondCategory}</li>))}</ul>
     </>
   );
 }
@@ -39,9 +49,12 @@ export default withLayout(Home);
 
 export const getStaticProps: GetStaticProps<HomeProps> = async () => {
   const firstCategory = 0;
-  const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
-    firstCategory
-  });
+  const { data: menu } = await axios.post<MenuItem[]>(
+    process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find',
+    {
+      firstCategory,
+    },
+  );
   return {
     props: {
       menu,
@@ -50,7 +63,7 @@ export const getStaticProps: GetStaticProps<HomeProps> = async () => {
   };
 };
 
-interface HomeProps extends Record<string, unknown>{
+interface HomeProps extends Record<string, unknown> {
   menu: MenuItem[];
   firstCategory: number;
 }
