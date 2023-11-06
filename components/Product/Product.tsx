082868b5fa2b886453d68f3589a88b2dc806af74
webberbond <sergey.zrch@gmail.com>
2023-11-06 11:05:36 +0500

    Product component - styles

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -1,27 +1,40 @@
 import { ProductProps } from './Product.props';
 import styles from './Product.module.css';
 import cn from 'classnames';
-import { Button, Card, Rating, Tag } from '..';
+import { Card } from '../Card/Card';
+import { Rating } from '../Rating/Rating';
+import { Tag } from '../Tag/Tag';
+import { Button } from '../Button/Button';
+import { priceRu } from '../../helpers/helpers';
+import { Divider } from '../Divider/Divider';
 
 export const Product = ({ product, className, ...props }: ProductProps) => {
   return (
     <Card className={styles.product}>
       <div className={styles.logo}>
-        {' '}
         <img
           src={process.env.NEXT_PUBLIC_DOMAIN + product.image}
           alt={product.title}
         />
       </div>
       <div className={styles.title}>{product.title}</div>
-      <div className={styles.price}>{product.price}</div>
-      <div className={styles.credit}>{product.credit}</div>
+      <div className={styles.price}>
+        {priceRu(product.price)}
+        {product.oldPrice && (
+          <Tag className={styles.oldPrice} color="green">
+            {priceRu(product.price - product.oldPrice)}
+          </Tag>
+        )}
+      </div>
+      <div className={styles.credit}>
+        {priceRu(product.credit)}/<span className={styles.month}>мес</span>
+      </div>
       <div className={styles.rating}>
-        <Rating rating={product.reviewAvg && product.initialRating} />
+        <Rating rating={product.reviewAvg ?? product.initialRating} />
       </div>
       <div className={styles.tags}>
         {product.categories.map((c) => (
-          <Tag key={c} color="ghost" size="s">
+          <Tag key={c} className={styles.category} color="ghost">
             {c}
           </Tag>
         ))}
@@ -29,27 +42,31 @@ export const Product = ({ product, className, ...props }: ProductProps) => {
       <div className={styles.priceTitle}>цена</div>
       <div className={styles.creditTitle}>кредит</div>
       <div className={styles.rateTitle}>{product.reviewCount} отзывов</div>
-      <div className={styles.hr}>
-        <hr />
-      </div>
+      <Divider className={styles.hr} />
       <div className={styles.description}>{product.description}</div>
-      <div className={styles.feature}>фичи </div>
+      <div className={styles.feature}>фичи</div>
       <div className={styles.advBlock}>
-        <div>
-          <div className={styles.advantages}>Преимущества</div>
-          {product.advantages}
-        </div>
-        <div>
-          <div className={styles.disadvantages}>Недостатки</div>
-          {product.disadvantages}
-        </div>
+        {product.advantages && (
+          <div className={styles.advantages}>
+            <div className={styles.advTitle}>Преимущества</div>
+            <div>{product.advantages}</div>
+          </div>
+        )}
+        {product.disadvantages && (
+          <div className={styles.disadvantages}>
+            <div className={styles.advTitle}>Недостатки</div>
+            <div>{product.disadvantages}</div>
+          </div>
+        )}
       </div>
-      <div className={styles.hr}>
-        <hr />
-      </div>
+      <Divider className={styles.hr} />
       <div className={styles.actions}>
         <Button appearance="primary">Узнать подробнее</Button>
-        <Button appearance="ghost" arrow={'right'}>
+        <Button
+          appearance="ghost"
+          arrow={'right'}
+          className={styles.reviewButton}
+        >
           Читать отзывы
         </Button>
       </div>
