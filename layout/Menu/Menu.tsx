fcb657150a7f2f33f9236f8676510cc1f2cf2268
webberbond <sergey.zrch@gmail.com>
2023-11-09 17:36:30 +0500

    Menu accessibility with keyboard

--- layout/Menu/Menu.tsx
+++ layout/Menu/Menu.tsx
@@ -1,6 +1,6 @@
 import styles from './Menu.module.css';
 import cn from 'classnames';
-import { useContext } from 'react';
+import { useContext, KeyboardEvent } from 'react';
 import { AppContext } from '../../context/app.context';
 import { FirstLevelMenuItem, PageItem } from '../../interfaces/menu.interface';
 import Link from 'next/link';
@@ -19,21 +19,15 @@ export const Menu = () => {
         staggerChildren: 0.1,
       },
     },
-    hidden: {
-      marginBottom: 0,
-    },
+    hidden: { marginBottom: 0 },
   };
   const variantsChildren = {
     visible: {
       opacity: 1,
       height: 29,
     },
-    hidden: {
-      opacity: 0,
-      height: 0,
-    },
+    hidden: { opacity: 0, height: 0 },
   };
-
   const openSecondLevel = (secondCategory: string) => {
     setMenu &&
       setMenu(
@@ -46,20 +40,29 @@ export const Menu = () => {
       );
   };
 
+  const openSecondLevelKey = (key: KeyboardEvent, secondCategory: string) => {
+    if (key.code == 'Space' || key.code == 'Enter') {
+      key.preventDefault();
+      openSecondLevel(secondCategory);
+    }
+  };
+
   const buildFirstLevel = () => {
     return (
       <>
         {firstLevelMenu.map((m) => (
           <div key={m.route}>
             <Link href={`/${m.route}`}>
-              <div
-                className={cn(styles.firstLevel, {
-                  [styles.firstLevelActive]: m.id == firstCategory,
-                })}
-              >
-                {m.icon}
-                <span>{m.name}</span>
-              </div>
+              <a>
+                <div
+                  className={cn(styles.firstLevel, {
+                    [styles.firstLevelActive]: m.id == firstCategory,
+                  })}
+                >
+                  {m.icon}
+                  <span>{m.name}</span>
+                </div>
+              </a>
             </Link>
             {m.id == firstCategory && buildSecondLevel(m)}
           </div>
@@ -67,7 +70,6 @@ export const Menu = () => {
       </>
     );
   };
-
   const buildSecondLevel = (menuItem: FirstLevelMenuItem) => {
     return (
       <div className={styles.secondBlock}>
@@ -80,6 +82,10 @@ export const Menu = () => {
           return (
             <div key={m._id.secondCategory}>
               <div
+                tabIndex={0}
+                onKeyDown={(key: KeyboardEvent) =>
+                  openSecondLevelKey(key, m._id.secondCategory)
+                }
                 className={styles.secondLevel}
                 onClick={() => openSecondLevel(m._id.secondCategory)}
               >
@@ -92,7 +98,7 @@ export const Menu = () => {
                 animate={m.isOpened ? 'visible' : 'hidden'}
                 className={cn(styles.secondLevelBlock)}
               >
-                {buildThirdLevel(m.pages, menuItem.route)}
+                {buildThirdLevel(m.pages, menuItem.route, m.isOpened ?? false)}
               </motion.div>
             </div>
           );
@@ -101,20 +107,26 @@ export const Menu = () => {
     );
   };
 
-  const buildThirdLevel = (pages: PageItem[], route: string) => {
+  const buildThirdLevel = (
+    pages: PageItem[],
+    route: string,
+    isOpened: boolean,
+  ) => {
     return pages.map((p) => (
       <motion.div key={p._id} variants={variantsChildren}>
-        <Link
-          href={`/${route}/${p.alias}`}
-          className={cn(styles.thirdLevel, {
-            [styles.thirdLevelActive]: `/${route}/${p.alias}` == router.asPath,
-          })}
-        >
-          {p.category}
+        <Link href={`/${route}/${p.alias}`}>
+          <a
+            tabIndex={isOpened ? 0 : -1}
+            className={cn(styles.thirdLevel, {
+              [styles.thirdLevelActive]:
+                `/${route}/${p.alias}` == router.asPath,
+            })}
+          >
+            {p.category}
+          </a>
         </Link>
       </motion.div>
     ));
   };
-
   return <div className={styles.menu}>{buildFirstLevel()}</div>;
 };
