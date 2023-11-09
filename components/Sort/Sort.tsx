b047beecbb45f20913cef63c2bf47aa28b9f948d
webberbond <sergey.zrch@gmail.com>
2023-11-09 17:53:35 +0500

    Sort accessibility

--- components/Sort/Sort.tsx
+++ components/Sort/Sort.tsx
@@ -2,11 +2,15 @@ import { SortEnum, SortProps } from './Sort.props';
 import styles from './Sort.module.css';
 import SortIcon from './sort.svg';
 import cn from 'classnames';
-
-export const Sort = ({ sort, setSort, className, ...props }: SortProps) => {
+export const Sort = ({
+  sort,
+  setSort,
+  className,
+  ...props
+}: SortProps): JSX.Element => {
   return (
     <div className={cn(styles.sort, className)} {...props}>
-      <span
+      <button
         onClick={() => setSort(SortEnum.Rating)}
         className={cn({
           [styles.active]: sort == SortEnum.Rating,
@@ -14,16 +18,16 @@ export const Sort = ({ sort, setSort, className, ...props }: SortProps) => {
       >
         <SortIcon className={styles.sortIcon} />
         По рейтингу
-      </span>
-      <span
+      </button>
+      <button
         onClick={() => setSort(SortEnum.Price)}
         className={cn({
           [styles.active]: sort == SortEnum.Price,
         })}
       >
         <SortIcon className={styles.sortIcon} />
-        По&nbsp;цене
-      </span>
+        По цене
+      </button>
     </div>
   );
 };
